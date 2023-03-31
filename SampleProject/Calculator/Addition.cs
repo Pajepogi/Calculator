5df893fec9c1ca667e46929444d18ffec92332a2
Deo <deorito.halago.jr@contitech.ph>
2023-03-31 17:47:47 +0800

    Addition Function

---
+++ SampleProject/Calculator/Addition.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Addition
+    {
+        public int firstVal { get; set; }
+        public int SecondVal { get; set; }
+    }
+}
